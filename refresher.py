3ba2025afbbc6209b2ca434249ead15c5d9941d6
Pijatow <weirdpijatow@gmail.com>
2021-05-09 12:43:25 +0430

    added refresh frequencyto refresher.py

--- refresher.py
+++ refresher.py
@@ -3,8 +3,8 @@ import time
 
 print('start in 5 seconds')
 time.sleep(5)
-
+refresh_frequency = int(input('refresh frequency: '))
 while True:
     pag.press('f5')
     pag.press('enter')
-    time.sleep(2)
+    time.sleep(refresh_frequency)
