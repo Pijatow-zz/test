7df796f572ee8edbaff053cc71796e98368db2a2
Pijatow <weirdpijatow@gmail.com>
2021-05-09 13:33:00 +0430

    Add colorised output feature to refresher.py

--- refresher.py
+++ refresher.py
@@ -1,10 +1,15 @@
 import pyautogui as pag
 import time
+from colorama import Fore, Back, Style
 
-print('start in 5 seconds')
+refresh_frequency = int(
+    input(Fore.BLACK + Back.WHITE + 'refresh frequency:' + Style.RESET_ALL + ' '))
+
+print(Back.BLUE + Fore.CYAN + 'start in 5 seconds' + Style.RESET_ALL)
 time.sleep(5)
-refresh_frequency = int(input('refresh frequency: '))
+
 while True:
     pag.press('f5')
     pag.press('enter')
     time.sleep(refresh_frequency)
+print(Fore.GREEN + 'done')
